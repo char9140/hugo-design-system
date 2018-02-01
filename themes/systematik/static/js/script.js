521ae5c83535595c5c96078a44eac2709999288c
CharlotteRN <charlotte.r.nielsen@hotmail.com>
2018-02-01 18:00:22 +0100

    menu virker responsivt - scss slettet

--- themes/systematik/static/js/script.js
+++ themes/systematik/static/js/script.js
@@ -7,3 +7,13 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
